77977163b8622757974f975247a970ac4a3815a8
Timothy Gaither <timjgaither@gmail.com>
2022-02-17 15:56:09 +0100

    push

---
+++ main.js
@@ -0,0 +1,27 @@
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            
+            mailCasuali: '',
+           
+        },
+
+        mounted() {
+
+           
+            
+            /* genero dei Mail casuali*/
+            for(let i=0;i<10;i++){
+
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((rispostaDelServer) => {
+                    this.mailCasuali+=rispostaDelServer.data.response;
+                    
+                    console.log(this.mailCasuali)
+                });
+            }
+
+        }
+    }
+)
